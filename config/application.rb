728e8a46797dfccac3a2602c5305fef417921cd5
Luka Vandervelden <lukc@upyum.com>
2018-03-18 19:45:00 +0100

    much i18n, very locales

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,9 @@ module Osem
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :en
+    config.i18n.available_locales = [:en, :fr]
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = 'utf-8'
