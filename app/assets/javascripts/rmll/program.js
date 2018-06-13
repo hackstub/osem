d0bce1a751d56cf25bd5dc7b700629185979a0a5
Axolotle <nchesnais@mailoo.org>
2018-06-13 20:48:45 +0200

    Improve flex disposition

--- app/assets/javascripts/rmll/program.js
+++ app/assets/javascripts/rmll/program.js
@@ -34,6 +34,9 @@ function formatJson(json) {
       }
 
       ev.date = date.getUTCHours() * 100 + date.getUTCMinutes();
+      ev.type = json.event_types.find(function (eventObj) {
+        if (eventObj.id === ev.event_type_id) return eventObj;
+      }).title.split(" ")[0];
       var daypartLen = daypart.length;
       if (daypartLen === 0) {
         daypart.push(ev);
@@ -58,7 +61,6 @@ function formatJson(json) {
 function buildTable(data, day) {
   var tbody = document.querySelector("table");
   var doc = document.createElement("tbody");
-  doc.classList.add("overflow");
   doc.id = day;
   if (day != "saturday") {
     doc.classList.add("hide");
@@ -69,7 +71,7 @@ function buildTable(data, day) {
       var tr = document.createElement("tr");
       var th = document.createElement("th");
       th.setAttribute("scope", "row");
-      th.innerHTML = data[i].track.name;
+      th.innerHTML = "<p>" + data[i].track.name + "</p>";
       tr.appendChild(th);
 
       var morning = document.createElement("td");
@@ -91,16 +93,35 @@ function buildTable(data, day) {
 }
 
 function buildArticle(ev) {
+  var container = document.createElement("div");
+  container.classList.add('event');
+
   var article = document.createElement("article");
   var title = document.createElement("h3");
   title.innerHTML = ev.title;
   var speaker = document.createElement("p");
   speaker.innerHTML = ev.speaker_names ;
+  var extra = document.createElement('aside')
+  var hour = document.createElement("span");
+  var type = document.createElement("span");
+  var time = '' + ev.date;
+  hour.innerHTML = time[0] + time[1] + "h" + time[2] + time[3];
+  type.innerHTML = ev.type;
+  extra.appendChild(hour);
+  extra.appendChild(type);
+
+  article.appendChild(extra);
   article.appendChild(title);
   article.appendChild(speaker);
   article.setAttribute("data-type", ev.event_type_id);
+  container.appendChild(article)
+  console.log(ev.type);
 
-  return article;
+  // var like = document.createElement("span");
+  // like.textContent = "<3";
+  // article.appendChild(like);
+
+  return container;
 }
 
 var baseUrl = "https://2018.rmll.info/api/v1/conferences/rmll2018/";
@@ -113,3 +134,5 @@ for (var i = 0; i < daysbutton.length; i++) {
     document.getElementById(e.target.dataset.toggle).classList.remove("hide");
   });
 }
+
+var events = document.getElementsByTagName("article")
