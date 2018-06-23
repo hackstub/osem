a71604c12280244a40037a209c896222cafafbe7
Axolotle <nchesnais@mailoo.org>
2018-06-23 15:23:23 +0200

    Add phone css style to calendar-list view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -228,7 +228,7 @@ module ApplicationHelper
   def add_bar(soft_type, name)
     haml_tag ".bar" do
       haml_tag "span", soft_type + " - " + name
-      haml_tag "a", "?", href:rmll_path
+      # haml_tag "a", "?", href:rmll_path
       haml_tag "a", "X", href:rmll_path
     end
   end
