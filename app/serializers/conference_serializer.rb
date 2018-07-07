de8ce71d9a741a958f54d225e55d08b353f27014
Quentin Gliech <quentingliech@gmail.com>
2018-07-07 13:52:03 +0200

    Add registration_url to events API

--- app/serializers/conference_serializer.rb
+++ app/serializers/conference_serializer.rb
@@ -1,4 +1,5 @@
 class ConferenceSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
   include ApplicationHelper
   include FormatHelper
   cache key: 'conference', expires_in: 3.hours
@@ -35,8 +36,14 @@ class ConferenceSerializer < ActiveModel::Serializer
                                                                                                 abstract_html: markdown(event_schedule.event.abstract),
                                                                                                 description: event_schedule.event.description,
                                                                                                 is_highlight: event_schedule.event.is_highlight,
-                                                                                                require_registration:  event_schedule.event.require_registration,
-                                                                                                max_attendees:  event_schedule.event.max_attendees,
+                                                                                                require_registration: event_schedule.event.require_registration,
+                                                                                                max_attendees: event_schedule.event.max_attendees,
+                                                                                                registration_url: url_for(controller: 'proposals',
+                                                                                                                          action: 'registrations',
+                                                                                                                          conference_id: object.short_title,
+                                                                                                                          id: event_schedule.event.id,
+                                                                                                                          only_path: true
+                                                                                                                         ),
                                                                                                 start_time: event_schedule.start_time,
                                                                                                 event_type_id: event_schedule.event.event_type.id,
                                                                                                 difficulty_level_id: event_schedule.event.difficulty_level_id,
